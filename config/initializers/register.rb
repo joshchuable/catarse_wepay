429fd54e3d8e1e179ba023902d9293334ae01f2b
Joshua Chua <jc5157@truman.edu>
2014-11-15 17:42:48 -0600

    Life is so good

--- config/initializers/register.rb
+++ config/initializers/register.rb
@@ -1,14 +1,5 @@
 begin
-  PaymentEngines.register({
-    name: 'wepay',
-    review_path: ->(contribution) {
-      CatarseWepay::Engine.routes.url_helpers.review_wepay_path(contribution)
-    },
-    refund_path: ->(contribution) {
-      CatarseWepay::Engine.routes.url_helpers.refund_wepay_path(contribution)
-    },
-    locale: 'en'
-  })
+  PaymentEngines.register(CatarseWepay::PaymentEngine.new)
 rescue Exception => e
   puts "Error while registering payment engine: #{e}"
 end
--- lib/catarse_wepay.rb
+++ lib/catarse_wepay.rb
@@ -3,4 +3,5 @@ require "catarse_wepay/engine"
 require "catarse_wepay/payment_engine"
 
 module CatarseWepay
+
 end
